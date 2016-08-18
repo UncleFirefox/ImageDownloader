5f503d04f444be762e10b523ee31c51e99e01de8
Albert Rodriguez Franco <albertrfranco@gmail.com>
2016-08-18 20:25:44 +0200

    Including all the changes

---
+++ ImageDownloader/MainForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ImageDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberStartlabel = new System.Windows.Forms.Label();
+            this.numberEndlabel = new System.Windows.Forms.Label();
+            this.folderDestinationTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GeneralProgressBar = new System.Windows.Forms.ProgressBar();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.BrowseFilesButton = new System.Windows.Forms.Button();
+            this.imageFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.Downloadbutton = new System.Windows.Forms.Button();
+            this.numericUpDownStart = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownEnd = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numberStartlabel
+            // 
+            this.numberStartlabel.AutoSize = true;
+            this.numberStartlabel.Location = new System.Drawing.Point(48, 10);
+            this.numberStartlabel.Name = "numberStartlabel";
+            this.numberStartlabel.Size = new System.Drawing.Size(69, 13);
+            this.numberStartlabel.TabIndex = 0;
+            this.numberStartlabel.Text = "Number Start";
+            // 
+            // numberEndlabel
+            // 
+            this.numberEndlabel.AutoSize = true;
+            this.numberEndlabel.Location = new System.Drawing.Point(176, 9);
+            this.numberEndlabel.Name = "numberEndlabel";
+            this.numberEndlabel.Size = new System.Drawing.Size(66, 13);
+            this.numberEndlabel.TabIndex = 3;
+            this.numberEndlabel.Text = "Number End";
+            // 
+            // folderDestinationTextBox
+            // 
+            this.folderDestinationTextBox.Enabled = false;
+            this.folderDestinationTextBox.Location = new System.Drawing.Point(15, 78);
+            this.folderDestinationTextBox.Name = "folderDestinationTextBox";
+            this.folderDestinationTextBox.Size = new System.Drawing.Size(215, 20);
+            this.folderDestinationTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Destination Folder";
+            // 
+            // GeneralProgressBar
+            // 
+            this.GeneralProgressBar.Location = new System.Drawing.Point(15, 131);
+            this.GeneralProgressBar.Name = "GeneralProgressBar";
+            this.GeneralProgressBar.Size = new System.Drawing.Size(256, 23);
+            this.GeneralProgressBar.TabIndex = 7;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(15, 112);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(40, 13);
+            this.statusLabel.TabIndex = 8;
+            this.statusLabel.Text = "Status:";
+            // 
+            // BrowseFilesButton
+            // 
+            this.BrowseFilesButton.Location = new System.Drawing.Point(236, 78);
+            this.BrowseFilesButton.Name = "BrowseFilesButton";
+            this.BrowseFilesButton.Size = new System.Drawing.Size(36, 23);
+            this.BrowseFilesButton.TabIndex = 9;
+            this.BrowseFilesButton.Text = "...";
+            this.BrowseFilesButton.UseVisualStyleBackColor = true;
+            this.BrowseFilesButton.Click += new System.EventHandler(this.BrowseFilesButton_Click);
+            // 
+            // imageFolderBrowser
+            // 
+            this.imageFolderBrowser.Description = "Select destination path for images";
+            // 
+            // Downloadbutton
+            // 
+            this.Downloadbutton.Location = new System.Drawing.Point(105, 178);
+            this.Downloadbutton.Name = "Downloadbutton";
+            this.Downloadbutton.Size = new System.Drawing.Size(75, 23);
+            this.Downloadbutton.TabIndex = 10;
+            this.Downloadbutton.Text = "Download";
+            this.Downloadbutton.UseVisualStyleBackColor = true;
+            this.Downloadbutton.Click += new System.EventHandler(this.Downloadbutton_Click);
+            // 
+            // numericUpDownStart
+            // 
+            this.numericUpDownStart.Location = new System.Drawing.Point(31, 26);
+            this.numericUpDownStart.Name = "numericUpDownStart";
+            this.numericUpDownStart.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDownStart.TabIndex = 11;
+            // 
+            // numericUpDownEnd
+            // 
+            this.numericUpDownEnd.Location = new System.Drawing.Point(155, 26);
+            this.numericUpDownEnd.Name = "numericUpDownEnd";
+            this.numericUpDownEnd.Size = new System.Drawing.Size(99, 20);
+            this.numericUpDownEnd.TabIndex = 12;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 213);
+            this.Controls.Add(this.numericUpDownEnd);
+            this.Controls.Add(this.numericUpDownStart);
+            this.Controls.Add(this.Downloadbutton);
+            this.Controls.Add(this.BrowseFilesButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.GeneralProgressBar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.folderDestinationTextBox);
+            this.Controls.Add(this.numberEndlabel);
+            this.Controls.Add(this.numberStartlabel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jeil\'s Image Downloader";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownEnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberStartlabel;
+        private System.Windows.Forms.Label numberEndlabel;
+        private System.Windows.Forms.TextBox folderDestinationTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar GeneralProgressBar;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button BrowseFilesButton;
+        private System.Windows.Forms.FolderBrowserDialog imageFolderBrowser;
+        private System.Windows.Forms.Button Downloadbutton;
+        private System.Windows.Forms.NumericUpDown numericUpDownStart;
+        private System.Windows.Forms.NumericUpDown numericUpDownEnd;
+    }
+}
+
---
+++ ImageDownloader/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageDownloader
+{
+    public partial class MainForm : Form
+    {
+        int numbphotos;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            numericUpDownStart.Maximum = decimal.MaxValue;
+            numericUpDownEnd.Maximum = decimal.MaxValue;
+        }
+
+        #region Delegate
+
+        public delegate void DelegateDownloadImage(int index);
+        
+        #endregion
+
+        private void BrowseFilesButton_Click(object sender, EventArgs e)
+        {
+            if (imageFolderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                folderDestinationTextBox.Text = imageFolderBrowser.SelectedPath;
+            }
+        }
+
+        private void Downloadbutton_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(folderDestinationTextBox.Text))
+            {
+                MessageBox.Show("The path specified does not exist", "Path problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (numericUpDownStart.Value > numericUpDownEnd.Value)
+            {
+                MessageBox.Show("Start number sequence is greater than end sequence", "Sequence problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            numbphotos = (int)(numericUpDownEnd.Value - numericUpDownStart.Value);
+            
+            GeneralProgressBar.Value = 0;
+            GeneralProgressBar.Minimum = 0;
+            GeneralProgressBar.Maximum = numbphotos+1;
+            Downloadbutton.Enabled = false;
+
+            DelegateDownloadImage delImage = new DelegateDownloadImage(DownloadImageDelegated);
+
+            for (int i = (int)numericUpDownStart.Value; i <= (int)numericUpDownEnd.Value; i++)
+            {
+                delImage.BeginInvoke(i, DownloadImageCallback, null);
+            }
+        }
+
+        #region Download Image Region
+
+        public void DownloadImageDelegated(int index)
+        {
+            Image img = DownloadImage(Properties.Settings.Default.ImageBaseURL + index);
+            
+            if (img != null && !img.Size.IsEmpty)
+                //Try to save the image
+                img.Save(Path.Combine(folderDestinationTextBox.Text, 
+                         Properties.Settings.Default.ImageNameBase+index+Properties.Settings.Default.ImageExtension)
+                );
+        }
+
+        public delegate void UpdateProgressDelegate();
+
+        public void DownloadImageCallback(IAsyncResult ar)
+        {
+            //Increment the progressbar
+            UpdateProgressDelegate UpdateProgress = new UpdateProgressDelegate(UpdateProgressBar);
+            GeneralProgressBar.Invoke(UpdateProgress);
+        }
+
+        private void UpdateProgressBar()
+        {
+            GeneralProgressBar.Value += 1;
+
+            if (GeneralProgressBar.Value == GeneralProgressBar.Maximum)
+            {
+                Downloadbutton.Enabled = true;
+                MessageBox.Show("Process finished succesfully!");
+            }
+        }
+
+        /// <summary>
+        /// Function to download Image from website
+        /// </summary>
+        /// <param name="_URL">URL address to download image</param>
+        /// <returns>Image</returns>
+        public Image DownloadImage(string _URL)
+        {
+            Image _tmpImage = null;
+
+            try
+            {
+                // Open a connection
+                System.Net.HttpWebRequest _HttpWebRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(_URL);
+
+                _HttpWebRequest.AllowWriteStreamBuffering = true;
+
+                // You can also specify additional header values like the user agent or the referer: (Optional)
+                _HttpWebRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)";
+                _HttpWebRequest.Referer = "http://www.google.com/";
+
+                // set timeout for 20 seconds (Optional)
+                _HttpWebRequest.Timeout = 20000;
+
+                // Request response:
+                System.Net.WebResponse _WebResponse = _HttpWebRequest.GetResponse();
+
+                // Open data stream:
+                System.IO.Stream _WebStream = _WebResponse.GetResponseStream();
+
+                // convert webstream to image
+                _tmpImage = Image.FromStream(_WebStream);
+
+                // Cleanup
+                _WebResponse.Close();
+                _WebResponse.Close();
+            }
+            catch (Exception _Exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}", _Exception.ToString());
+                return null;
+            }
+
+            return _tmpImage;
+        }
+
+        #endregion   
+    }
+}
